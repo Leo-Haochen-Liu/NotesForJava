4289af5681d186ff0771ce9d55a65f39c52a284d
haoch <Haochen.Liu@anu.edu.au>
2024-11-19 15:00:24 +0800

    Han Shunping Java practice 5 - A-Z array updated

--- src/BasicLanguageUse/ContrlStructures/Practice5_AToZ.java
+++ src/BasicLanguageUse/ContrlStructures/Practice5_AToZ.java
@@ -3,18 +3,55 @@ package BasicLanguageUse.ContrlStructures;
 public class Practice5_AToZ {
 
     /*
-    * print out a-z and A-Z
-    * */
+     * print out a-z and A-Z
+     * */
+
+    /*
+     * create an array to restore 'A'-'Z'.
+     * then use for loop to print them out
+     * */
 
     public static void main(String[] args) {
+
+        // 10个一行打印a-z
+        int count = 0;
         for (int i = 97; i <= 122; i++) {
-            System.out.println((char) i);
+            if (count == 10) {
+                System.out.println("");
+                count = 0;
+            }
+            System.out.print((char) i);
+            count++;
         }
 
-        System.out.println("");
+        // 10个一行打印A-Z
+        count = 0;
+        System.out.println("\n");
 
         for (int i = 65; i <= 90; i++) {
-            System.out.println((char) i);
+            if (count == 10) {
+                System.out.println("");
+                count = 0;
+            }
+            System.out.print((char) i);
+            count++;
+        }
+
+        // A-Z数组存储并打印
+        count = 0;
+        System.out.println("\n");
+
+        char[] AToZ = new char[26];
+        for (int i = 0; i < 26; i++) {
+            AToZ[i] = (char) ('A' + i);
+        }
+        for (int i = 0; i < 26; i++) {
+            if (count == 10) {
+                System.out.println("");
+                count = 0;
+            }
+            System.out.print(AToZ[i]);
+            count++;
         }
     }
 }
